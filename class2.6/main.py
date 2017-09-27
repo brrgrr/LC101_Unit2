ae6a4810c62948fc6bb765c87f5658ee13908333
Eric Berger <eric@brrgrr.com>
2017-09-27 13:26:09 -0500

    class2.6 prep and refactor

--- class2.6/main.py
+++ class2.6/main.py
@@ -4,9 +4,46 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+title = ''
+
+
 @app.route("/")
 def index():
-    return render_template('display.html')
+    title = 'Hello Form'
+    return render_template(
+        'hello_form.html',
+        title=title
+    )
+
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    title = 'Hello Greeting'
+    first_name = request.form['first_name']
+    return render_template(
+        'hello_greeting.html',
+        title=title,
+        name=first_name
+    )
+
+
+tasks = []
+
+
+@app.route('/todos', methods=['POST', 'GET'])
+def todos():
+    title = 'TODOs'
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    return render_template(
+        'todos.html',
+        title=title,
+        tasks=tasks
+    )
+
 
 if __name__ == '__main__':
     app.run()
